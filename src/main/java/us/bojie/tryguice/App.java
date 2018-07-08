9d0e0c6cba7dbf9db6928f757da5607047e11f63
Bojie Jiang <jiangbojie@gmail.com>
2018-07-08 11:07:20 -0700

    Applets

--- src/main/java/us/bojie/tryguice/App.java
+++ src/main/java/us/bojie/tryguice/App.java
@@ -1,11 +1,7 @@
 package us.bojie.tryguice;
 
-import com.google.common.base.Joiner;
 import com.google.inject.Guice;
-import com.google.inject.Key;
-import com.google.inject.TypeLiteral;
-
-import java.util.Map;
+import com.google.inject.Injector;
 
 import us.bojie.tryguice.helloworlddemo.CommandLineModule;
 
@@ -19,18 +15,9 @@ public class App {
      * @param args
      */
     public static void main(String[] args) {
-        Map<String, MyApplet> applets =
-                Guice.createInjector(new MainModule(),
-                        new CommandLineModule(args))
-                        .getInstance(Key.get(
-                                new TypeLiteral<Map<String, MyApplet>>() {
-                                }));
-        if (args.length == 0 || !applets.containsKey(args[0])) {
-            System.out.println("Unable to find applet."
-                    + "Valid applets are "
-                    + Joiner.on(", ").join(applets.keySet()));
-            return;
-        }
-        applets.get(args[0]).run();
+        Injector injector = Guice.createInjector(new MainModule(),
+                new CommandLineModule(args));
+
+        Applets.get(injector, args[0]).run();
     }
 }
--- src/main/java/us/bojie/tryguice/helloworlddemo/HelloWorldModule.java
+++ src/main/java/us/bojie/tryguice/helloworlddemo/HelloWorldModule.java
@@ -2,19 +2,21 @@ package us.bojie.tryguice.helloworlddemo;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
-import com.google.inject.multibindings.MapBinder;
 
 import java.io.PrintStream;
 import java.util.List;
 
-import us.bojie.tryguice.MyApplet;
+import us.bojie.tryguice.Applets;
 
 public class HelloWorldModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        MapBinder.newMapBinder(binder(), String.class, MyApplet.class)
-                .addBinding("hello").to(StringWritingApplet.class);
+
+        Applets.register(binder()).named("hello")
+                .to(StringWritingApplet.class);
+//        MapBinder.newMapBinder(binder(), String.class, MyApplet.class)
+//                .addBinding("hello").to(StringWritingApplet.class);
 //        bind(MyApplet.class).annotatedWith(Names.named("hello")).to(StringWritingApplet.class);
         bind(MyDestination.class).to(PrintStreamWriter.class);
         bind(PrintStream.class).toInstance(System.out);
