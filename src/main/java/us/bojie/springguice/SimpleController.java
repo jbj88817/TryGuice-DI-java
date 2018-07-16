4e03eea504ffe9d3fa3eb45a98e2757a8179214d
Bojie Jiang <jiangbojie@gmail.com>
2018-07-15 23:48:18 -0700

    Starter

---
+++ src/main/java/us/bojie/springguice/SimpleController.java
@@ -0,0 +1,13 @@
+package us.bojie.springguice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleController {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleController.class, args);
+    }
+}
+
